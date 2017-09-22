ac2283bccce2f5472d830b5400ffcb8c60343490
[Jesper] <[jp222vq@student.lnu.se]>
2017-09-22 16:28:35 -0700

    Login error handling

--- controller/LoginCtrl.php
+++ controller/LoginCtrl.php
@@ -2,23 +2,39 @@
 
 class LoginCtrl {
 
-	/**
-	 * Save new user
-	 *
-	 * Should be called after a register attempt has successfully been made
-	 *
-	 * @return void
-	 */
-	public function loginUser(User $userDetails) {
+	private $user;
 
-		$userDetails->loginUser($_POST['LoginView::UserName'], $_POST['LoginView::Password']);
+	public function loginUser(User $user) {
+		$this->user = $user;
+		$this->validator();
+		$this->findUserInDb();
 	}
 
-	private function checkUsernameField() {
+	public function findUserInDb(User $userDetails) {
+
+		$this->user->loginUser($_POST['LoginView::UserName'], $_POST['LoginView::Password']);
+		header("Location:index.php?LoginView::Message=Successful!.");
+	}
+
+	private function validator() {
+		if ($this->checkUsernameInput()) {
+			header("Location:" . $_SERVER['PHP_SELF'] . "?LoginView::Message=Username is missing");
+		} else if ($this->checkPasswordInput()) {
+			header("Location:" . $_SERVER['PHP_SELF'] . "?LoginView::Message=Password is missing");
+		} else if ($this->checkUsernameExists()) {
+			header("Location:" . $_SERVER['PHP_SELF'] . "?LoginView::Message=Wrong name or password");
+		}
+	}
+
+	private function checkUsernameInput() {
 		return strlen($_POST['LoginView::UserName']) < 3;
 	}
 
-	private function checkPasswordField() {
+	private function checkPasswordInput() {
 		return strlen($_POST['LoginView::Password']) < 6;
+	}
+
+	private function checkUsernameExists() {
+		return $this->user->userExists($_POST['LoginView::UserName']);
 	}
 }
